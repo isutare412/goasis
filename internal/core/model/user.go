ffca13f9bd07fcd4cfb5cf709fc9cb087d40cdf6
Redshore <isutare412@gmail.com>
2024-09-03 14:51:47 +0900

    feat: add user repository

--- internal/core/model/user.go
+++ internal/core/model/user.go
@@ -12,8 +12,15 @@ type User struct {
 	UpdatedAt time.Time
 	DeletedAt gorm.DeletedAt
 
-	Nickname    string  `gorm:"not null; size:64; check:nickname <> ''"`
-	FamilyName  *string `gorm:"size:128"`
-	GivenName   *string `gorm:"size:128"`
-	DateOfBirth *time.Time
+	Nickname     string  `gorm:"not null; size:64; check:nickname <> ''"`
+	FamilyName   *string `gorm:"size:128"`
+	MiddleName   *string `gorm:"size:128"`
+	GivenName    *string `gorm:"size:128"`
+	*DateOfBirth `gorm:"embeddedPrefix:birth_"`
+}
+
+type DateOfBirth struct {
+	Year  *int        `gorm:"type:smallint"`
+	Month *time.Month `gorm:"type:smallint"`
+	Day   *int        `gorm:"type:smallint"`
 }
---
+++ internal/postgres/user_repository.go
@@ -0,0 +1,70 @@
+package postgres
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"gorm.io/gorm"
+
+	"github.com/isutare412/goasis/internal/core/model"
+	"github.com/isutare412/goasis/internal/pkgerr"
+)
+
+type UserRepository struct {
+	db *gorm.DB
+}
+
+func NewUserRepository(client *Client) *UserRepository {
+	return &UserRepository{
+		db: client.db,
+	}
+}
+
+func (r *UserRepository) CreateUser(ctx context.Context, user *model.User) (created *model.User, err error) {
+	db := getTxOrDB(ctx, r.db).WithContext(ctx)
+
+	if err := db.Create(user).Error; err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
+func (r *UserRepository) UpdateUser(ctx context.Context, user *model.User) (updated *model.User, err error) {
+	db := getTxOrDB(ctx, r.db).WithContext(ctx)
+
+	if err := db.Save(user).Error; err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
+func (r *UserRepository) DeleteUser(ctx context.Context, id int64) error {
+	db := getTxOrDB(ctx, r.db).WithContext(ctx)
+
+	if err := db.Delete(&model.User{}, id).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *UserRepository) GetUser(ctx context.Context, id int64) (*model.User, error) {
+	db := getTxOrDB(ctx, r.db).WithContext(ctx)
+
+	var user model.User
+	err := db.First(&user, id).Error
+	switch {
+	case errors.Is(err, gorm.ErrRecordNotFound):
+		return nil, pkgerr.CodeError{
+			Code:      pkgerr.CodeNotFound,
+			Err:       err,
+			ClientMsg: fmt.Sprintf("user with id %d not found", id),
+		}
+	case err != nil:
+		return nil, err
+	}
+
+	return &user, nil
+}
